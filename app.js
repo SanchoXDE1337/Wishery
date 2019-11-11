b242378237e22400ceb7477e05fe8b109a80d228
Alexander Dolgov <sanhoxd@gmail.com>
2019-11-12 01:45:15 +0300

    :bug:

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ require('dotenv/config')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
-
+const cors = require('cors')
 app.use(express.json())
-
+app.use(cors())
 // import routes
 const postsRoute = require('./routes/posts')
 
