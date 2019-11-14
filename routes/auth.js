d33531d0015e69fd0bf074f9340b1d67e1471a1a
Alexander Dolgov <sanhoxd@gmail.com>
2019-11-14 05:24:46 +0300

    fixin'  :bug:

--- routes/auth.js
+++ routes/auth.js
@@ -49,10 +49,10 @@ router.post('/login', async (req, res) => {
     }
 
     const validPass = await bcrypt.compare(req.body.password, user.password)
-    if(!validPass) return res.status(400).send('Invalid password')
+    if(!validPass) return res.status(400).send("Invalid password")
 
     const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET, { expiresIn: '30m'})
-    res.header('auth-token', token).send("Logged in!")
+    res.header('auth-token', token).send([token, user._id])
 })
 
 /*// routers
