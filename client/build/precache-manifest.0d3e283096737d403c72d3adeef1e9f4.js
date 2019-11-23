bfadc188e9260416d952f698fab63323e181aea5
Alexander Dolgov <sanhoxd@gmail.com>
2019-11-24 02:27:43 +0300

    fixx

---
+++ client/build/precache-manifest.0d3e283096737d403c72d3adeef1e9f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0c116b415a376da067376bb5c50146",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb7828c5cc14fc6ca164",
+    "url": "/static/css/2.5bb98248.chunk.css"
+  },
+  {
+    "revision": "4aaf71acec8caf6fd69c",
+    "url": "/static/css/main.abb4506d.chunk.css"
+  },
+  {
+    "revision": "bb7828c5cc14fc6ca164",
+    "url": "/static/js/2.a8cce0af.chunk.js"
+  },
+  {
+    "revision": "4aaf71acec8caf6fd69c",
+    "url": "/static/js/main.4e243e58.chunk.js"
+  },
+  {
+    "revision": "d16121376eca991f0000",
+    "url": "/static/js/runtime-main.83739810.js"
+  }
+]);
