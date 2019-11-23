40bf26c640b9d1fc9c93e883589691e53d13837b
Alexander Dolgov <sanhoxd@gmail.com>
2019-11-24 02:17:13 +0300

    fixx

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTo
 if (process.env.NODE_ENV === 'production') {
     server.use(express.static(path.join(__dirname, 'client/build')))
     server.get('*', (res, req) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
