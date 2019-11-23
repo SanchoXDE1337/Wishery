9ade0656bc9c8198cfc4d40ac4835611cd81dfcb
Alexander Dolgov <sanhoxd@gmail.com>
2019-11-24 02:51:01 +0300

    fixx

--- server.js
+++ server.js
@@ -5,28 +5,28 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const path = require('path')
 
-const server = express()
+const app = express()
 
-server.use(express.json())
-server.use(cors())
+app.use(express.json())
+app.use(cors())
 
 const PORT = process.env.PORT || 8080
 
 // import routes
 const postsRoute = require('./routes/posts')
-server.use('/posts', postsRoute)
+app.use('/posts', postsRoute)
 
 const authRoute = require('./routes/auth')
-server.use('/user', authRoute)
+app.use('/user', authRoute)
 
 const privateRoute = require('./routes/private')
-server.use('/private', privateRoute)
+app.use('/private', privateRoute)
 
 const homeRoute = require('./routes/home')
-server.use('/', homeRoute)
+app.use('/', homeRoute)
 
 const commentRoute = require('./routes/comments')
-server.use('/comments', commentRoute)
+app.use('/comments', commentRoute)
 
 //connect to db
 mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false},
@@ -34,14 +34,14 @@ mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true, useUnifiedTo
 
 
 if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    server.use(express.static(path.join(__dirname, 'client/build')))
-    // Handle React routing, return all requests to React app
-    server.get('*', function (req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-    })
+    app.use(express.static('client/build'))
 }
 
-server.listen(PORT, () => {
+// Handle React routing, return all requests to React app
+app.get('*', (request, response) => {
+    response.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
+
+app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
