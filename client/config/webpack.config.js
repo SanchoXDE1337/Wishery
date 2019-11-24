656bb3764d22ecd62a936873f6f7a1d7a90ca336
Alexander Dolgov <sanhoxd@gmail.com>
2019-11-24 03:53:19 +0300

    fixx

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -348,13 +348,14 @@ module.exports = function(webpackEnv) {
                 formatter: require.resolve('react-dev-utils/eslintFormatter'),
                 eslintPath: require.resolve('eslint'),
                 resolvePluginsRelativeTo: __dirname,
-                
+
               },
               loader: require.resolve('eslint-loader'),
             },
           ],
           include: paths.appSrc,
         },
+        { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'], },
         {
           // "oneOf" will traverse all following loaders until one will
           // match the requirements. When no loader matches it will fall
@@ -381,7 +382,7 @@ module.exports = function(webpackEnv) {
                 customize: require.resolve(
                   'babel-preset-react-app/webpack-overrides'
                 ),
-                
+
                 plugins: [
                   [
                     require.resolve('babel-plugin-named-asset-import'),
@@ -423,7 +424,7 @@ module.exports = function(webpackEnv) {
                 cacheDirectory: true,
                 // See #6846 for context on why cacheCompression is disabled
                 cacheCompression: false,
-                
+
                 // If an error happens in a package, it's possible to be
                 // because it was compiled. Thus, we don't want the browser
                 // debugger to show the original code. Instead, the code
