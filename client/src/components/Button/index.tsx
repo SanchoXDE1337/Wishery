15a18e3281a11313708f3a3036559df33d5ed7e0
Alexander Dolgov <sanhoxd@gmail.com>
2019-11-24 04:52:50 +0300

    PRODUCTION!

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './styles.scss';
+import styles from './styles.module.scss';
 
 const Button: React.FC<React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>> = ({children, ...props}) => <button className={styles.root} {...props}>{children}</button>;
 export default Button
